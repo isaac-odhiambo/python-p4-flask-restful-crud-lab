2bb2e21ae7e3850fde40a9d3fb8c829bd4cb28b7
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-10-10 12:46:16 +0300

    done

--- server/app.py
+++ server/app.py
@@ -16,40 +16,44 @@ db.init_app(app)
 
 api = Api(app)
 
-
-class Plants(Resource):
-
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
-
-    def post(self):
-        data = request.get_json()
-
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            response_dict = plant.to_dict()
+            return make_response(jsonify(response_dict), 200)
+        else:
+            return make_response({"error": "Plant not found"}, 404)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        data = request.get_json()
+        for attr in data:
+            if hasattr(plant, attr):
+                setattr(plant, attr, data[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+
+            # Return an empty response to match the test expectations
+            return make_response('', 204)  # No Content
+
+        return make_response({"error": "Plant not found"}, 404)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
